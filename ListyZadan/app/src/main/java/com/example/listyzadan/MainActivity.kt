1705f25fafa500d8dfd1ebda7206bff25d693a27
W-Stefanska <stefanskaweronika86@gmail.com>
2024-11-17 21:08:08 +0100

    Added navController

--- ListyZadan/app/src/main/java/com/example/listyzadan/MainActivity.kt
+++ ListyZadan/app/src/main/java/com/example/listyzadan/MainActivity.kt
@@ -5,6 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.listyzadan.databinding.ActivityMainBinding
 import kotlin.random.Random
 
@@ -33,18 +37,27 @@ val Subjects = mutableListOf(
     Subject("Elektronika"),
     Subject("Algorytmy")
 )
+
 class MainActivity : AppCompatActivity() {
+    private val navController: NavController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                as NavHostFragment
+        navHostFragment.findNavController()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+        binding.bottomNavView.setupWithNavController(navController)
         for (i in 1..20) {
             val thyme = MutableList(Random.nextInt(1, 10)) {
                 Exercise(
